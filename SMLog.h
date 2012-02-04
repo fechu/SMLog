d36a039cc231df8edb8fcf2f8573d4c456922e19
Sandro Meier <sandro.meier@fidelisfactory.ch>
2012-02-04 19:12:50 +0100

    Implemented Alog, Dlog and UILog.

--- SMLog.h
+++ SMLog.h
@@ -5,7 +5,7 @@
 //  Copyright 2011 Fidelis Factory. All rights reserved.
 //
 
-
+// Logs only in DEBUG mode.
 #ifdef DEBUG
 #   define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
 #else
@@ -14,3 +14,6 @@
 
 // ALog always displays output regardless of the DEBUG setting
 #define ALog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+
+// Shows a UIAlertView with the logged Data. 
+#define UILog(fmt, ...) {UIAlertView *alert = [[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%s\n [Line %d] ", __PRETTY_FUNCTION__, __LINE__] message:[NSString stringWithFormat:fmt, ##__VA_ARGS__]  delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil]; [alert show]; }
