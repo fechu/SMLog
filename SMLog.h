ec59a4f04b0e703f7091f1ba40f2ad438c114538
Sandro Meier <sandro.meier@fidelisfactory.ch>
2012-05-07 13:05:15 +0200

    Added 5 Logging levels.

--- SMLog.h
+++ SMLog.h
@@ -5,15 +5,26 @@
 //  Copyright 2011 Fidelis Factory. All rights reserved.
 //
 
+/* 
+ The different Logging Levels for DLog
+ 
+ Use DLog\(.*\) to find all Logs and replace with DLog\(LEVEL_DEBUG, \1\)
+ */
+#define LEVEL_DEBUG     @"DEBUG"
+#define LEVEL_NOTICE    @"NOTICE"
+#define LEVEL_WARNING   @"WARNING"
+#define LEVEL_IMPORTANT @"IMPORTANT"
+#define LEVEL_ERROR     @"ERROR"
+
 // If this options is set to 1, Line number and funtion name are added to the log.  
 #define ADD_INFORMATION 0
 
 // Logs only in DEBUG mode.
 #ifdef DEBUG
 #   if ADD_INFORMATION 
-#       define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#       define DLog(level ,fmt, ...) NSLog((@"[%@]  %s [Line %d] " fmt), level, __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
 #   else
-#       define DLog(fmt, ...) NSLog((fmt), ##__VA_ARGS__);
+#       define DLog(level ,fmt, ...) NSLog((@"[%@]  " fmt), level, ##__VA_ARGS__);
 #   endif
 #else
 #   define DLog(...)
