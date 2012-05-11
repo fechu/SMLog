1dccee73128aaac759026d1e142f73cfb039901a
Sandro Meier <sandro.meier@fidelisfactory.ch>
2012-05-11 18:38:16 +0200

    Implemented SMLogger. Not all functionality is already implemented.

---
+++ SMLog.m
@@ -0,0 +1,159 @@
+/**
+ @file SMLog.m
+ 
+ @author Sandro Meier <sandro.meier@fidelisfactory.ch>
+ @date 10.5.2012
+ */
+
+#import "SMLog.h"
+
+@interface SMLogger ()
+
+/**
+ Returns the name as a string for level.
+ @param level The level you want the name for
+ */
++ (NSString *)nameForLogLevel:(SMLogLevel)level;
+
+@end
+
+
+
+
+
+@implementation SMLogger
+
+@synthesize logLevels;
+
+/**
+ The variable that contains the sharedLogger
+ */
+static SMLogger *aLogger;
+
+#pragma mark - Initialization
+
+- (id)init
+{
+    if (self = [super init])
+    {
+        self.logLevels = SMLogLevelDebug | SMLogLevelNotice | SMLogLevelWarning | SMLogLevelImportant | SMLogLevelError; 
+    }
+    
+    return self;
+}
+
++ (SMLogger *)sharedLogger
+{
+    if (aLogger == nil)
+    {
+        @synchronized(self) 
+        {
+            if (aLogger == nil)
+            {
+                // Create the shared logger
+                aLogger = [[SMLogger alloc] init];
+            }
+        }
+    }
+    return aLogger;
+}
+
+#pragma mark - Generic Logging
+
++ (void)log:(NSString *)text level:(SMLogLevel)level
+{
+    [[SMLogger sharedLogger] log:text level:level];
+}
+
+- (void)log:(NSString *)text level:(SMLogLevel)level
+{
+    NSLog(@"%@ %@", [SMLogger nameForLogLevel:level], text);
+}
+
++ (void)logDebug:(NSString *)text
+{
+    [[SMLogger sharedLogger] logDebug:text];
+}
+
+- (void)logDebug:(NSString *)text
+{
+    [self log:text level:SMLogLevelDebug];
+}
+
++ (void)logNotice:(NSString *)text
+{
+    [[SMLogger sharedLogger] logNotice:text];
+}
+
+- (void)logNotice:(NSString *)text
+{
+    [self log:text level:SMLogLevelNotice];
+}
+
++ (void)logWarning:(NSString *)text
+{
+    [[SMLogger sharedLogger] logWarning:text];
+}
+
+- (void)logWarning:(NSString *)text
+{
+    [self log:text level:SMLogLevelWarning];
+}
+
++ (void)logImportant:(NSString *)text
+{
+    [[SMLogger sharedLogger] logImportant:text];
+}
+
+- (void)logImportant:(NSString *)text
+{
+    [self log:text level:SMLogLevelImportant];
+}
+
++ (void)logError:(NSString *)text
+{
+    [[SMLogger sharedLogger] logError:text];
+}
+
+- (void)logError:(NSString *)text
+{
+    [self log:text level:SMLogLevelError];
+}
+
+#pragma mark - Names
+
++ (NSString *)nameForLogLevel:(SMLogLevel)level 
+{
+    switch (level) {
+        case SMLogLevelDebug:
+        {
+            return @"DEBUG    ";
+            break;
+        }   
+        case SMLogLevelNotice:
+        {
+            return @"NOTICE   ";
+            break;
+        }
+        case SMLogLevelWarning:
+        {
+            return @"WARNING  ";
+            break;
+        }
+        case SMLogLevelImportant:
+        {
+            return @"IMPORTANT";
+            break;
+        }
+        case SMLogLevelError:
+        {
+            return @"ERROR    ";
+            break;
+        }
+        default:
+            return @"";
+            break;
+    }
+}
+
+@end
